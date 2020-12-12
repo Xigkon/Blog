c25c75498b226a92ab1e8388e6d1bc49f69dbc33
Xigkon <chuanwangwu3769@gmail.com>
2020-12-12 21:57:27 +0800

    添加主页

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  title="Xigkon's Blog";
+  titleDescribe="千里之行，始于足下";
+  homeDescribe1 ="在这里，我会记录的学习中遇到的问题以及解决方法";
+  homeDescribe2 ="还有我奇怪的想法"
 
+  homeItem= [
+    {
+      text: "Front End",
+      url: "frontend",
+      time: "Dec 5 2020"
+    },{
+      text: "Back End",
+      url: "backend",
+      time: "Dec 5 2020"
+    },{
+     text: "Basis",
+      url: "basis" ,
+      time: "Dec 9 2020"
+    },{
+      text: "Tools",
+      url: "tools",
+      time: "Dec 10 2020"
+    },{
+      text: "Thinking",
+      url: "thinking",
+      time: "Dec 8 2020"
+
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
